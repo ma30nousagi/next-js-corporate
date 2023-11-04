b915e591c33d09d8f9590d98b92f17aa1f58766e
Muneto <punchkids3713@gmail.com>
2023-11-04 15:28:27 +0900

    about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,6 @@ import Header from 'src/components/Header/page';
 import { About } from 'src/components/About/page';
 import { Footer } from 'src/components/Footer/page';
 import type { Metadata } from 'next'
-import { useEffect } from 'react';
 
 
 export const metadata: Metadata = {
